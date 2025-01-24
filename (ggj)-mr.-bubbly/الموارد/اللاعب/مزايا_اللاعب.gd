1777c30e2a9e629b67fe56b9dc5237f1e1be01b2
SushieA1 <144395475+SushieA1@users.noreply.github.com>
2025-01-24 19:37:56 +0300

    Add files via upload

--- (ggj)-mr.-bubbly/الموارد/اللاعب/مزايا_اللاعب.gd
+++ (ggj)-mr.-bubbly/الموارد/اللاعب/مزايا_اللاعب.gd
@@ -1,7 +1,7 @@
 extends Resource
 class_name المزايا
 
-@export var size: float = 10
-@export var hp: float = 10
-@export var speed: float = 1
+@export var sizeMultiplier: float = 1
+@export var moistureMeter: float = 60
+@export var playerSpeed: float = 3
 @export var slip: float = 0

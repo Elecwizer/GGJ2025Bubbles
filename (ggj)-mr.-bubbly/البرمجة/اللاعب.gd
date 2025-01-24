2f02e9c54cf2cbd110ca83b813b01da6b956a227
mohammed-essa-alsultan <mohals10002@gmail.com>
2025-01-24 20:55:00 +0300

    8:54pm

--- (ggj)-mr.-bubbly/البرمجة/اللاعب.gd
+++ (ggj)-mr.-bubbly/البرمجة/اللاعب.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
+const FRIC = 0.1
 const JUMP_VELOCITY = 4.5
 
 
@@ -19,13 +20,13 @@ func _physics_process(delta: float) -> void:
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("a", "d", "w", "s")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = lerpf(velocity.x, 0, FRIC)
+		velocity.z = lerpf(velocity.z, 0, FRIC)
 
 	move_and_slide()

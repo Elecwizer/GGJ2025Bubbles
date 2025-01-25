4955273f394db07d70835dbc06a810f7bf336666
mohammed-essa-alsultan <mohals10002@gmail.com>
2025-01-25 18:51:54 +0300

    6:51

--- (ggj)-mr.-bubbly/البرمجة/air.gd
+++ (ggj)-mr.-bubbly/البرمجة/air.gd
@@ -7,9 +7,8 @@ func _ready() -> void:
 
 func Hit(body: Node3D) -> void:
 	if body.has_meta("Player"):
-		#body.emit_signal("PlaySound","res://SoundEffects/TouchingWater.mp3")
+		body.emit_signal("PlaySound","res://التأثيرات الصوتية/SoundEffects/TouchingWater.mp3")
 		body.playerProperties.onHit()
 		print(body.playerProperties.HP)
 		if body.playerProperties.dead == true:
 			body.emit_signal("PlaySound","res://التأثيرات الصوتية/SoundEffects/BackgroundMusic.mp3")
-			body.playerProperties.playerSpeed = 0
--- (ggj)-mr.-bubbly/الموارد/اللاعب/مزايا_اللاعب.gd
+++ (ggj)-mr.-bubbly/الموارد/اللاعب/مزايا_اللاعب.gd
@@ -19,7 +19,7 @@ var dead:bool = false
 signal die
 
 func onHit(damage:float = 3):
-	if HP > 0:
+	if HP >= 0:
 		HP -= damage
 	else:
 		dead = true
